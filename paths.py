1af2427d9d961191137f00ca52bfcb05ec7cf28b
ashokatgitworl <ashokssdn@gmail.com>
2025-11-30 17:41:14 +0530

    first commit

--- paths.py
+++ paths.py
@@ -1,34 +1,16 @@
 import os
-
-# ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# ENV_FPATH = os.path.join(ROOT_DIR, ".env")
-
-# CODE_DIR = os.path.join(ROOT_DIR, "code")
-
-# APP_CONFIG_FPATH = os.path.join(ROOT_DIR, "ITSMYGUIDE/config", "config.yaml")
-# PROMPT_CONFIG_FPATH = os.path.join(ROOT_DIR, "ITSMYGUIDE/config", "prompt_config.yaml")
-# OUTPUTS_DIR = os.path.join(ROOT_DIR, "ITSMYGUIDE/outputs")
-
-# DATA_DIR = os.path.join(ROOT_DIR, "ITSMYGUIDE\Source")
-# PUBLICATION_FPATH = os.path.join(DATA_DIR, "publication.md")
-
-# VECTOR_DB_DIR = os.path.join(OUTPUTS_DIR, "vector_db")
-# CHAT_HISTORY_DB_FPATH = os.path.join(OUTPUTS_DIR, "chat_history.db")
-# CHROMA_URLL = "http://chromadb-cloud-production.up.railway.app"
-# CHROMA_PORTT = "443"
-
-
 from pathlib import Path
 
 # Base directory (parent of the folder where this file lives)
 ROOT_DIR = Path(__file__).resolve().parent.parent
+APP_ROOT = Path(__file__).resolve().parent
 
 ENV_FPATH = ROOT_DIR / ".env"
 
 CODE_DIR = ROOT_DIR / "code"
 
-APP_CONFIG_FPATH = ROOT_DIR / "ITSMYGUIDE" / "config" / "config.yaml"
-PROMPT_CONFIG_FPATH = ROOT_DIR / "ITSMYGUIDE" / "config" / "prompt_config.yaml"
+APP_CONFIG_FPATH = APP_ROOT / "ITSMYGUIDE" / "config" / "config.yaml"
+PROMPT_CONFIG_FPATH = APP_ROOT / "ITSMYGUIDE" / "config" / "prompt_config.yaml"
 OUTPUTS_DIR = ROOT_DIR / "ITSMYGUIDE" / "outputs"
 
 DATA_DIR = ROOT_DIR / "ITSMYGUIDE" / "Source"
